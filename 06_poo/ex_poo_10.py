a6fb66db06ec41c1c2f7bde5cbdb6c30061fc2e2
Giovane Soares <soaresgeovane058@gmail.com>
2025-12-15 22:33:38 -0300

    utilizando de metodos magicos para melhorias na POO

--- 06_poo/ex_poo_10.py
+++ 06_poo/ex_poo_10.py
@@ -11,3 +11,33 @@ Além dos métodos mágicos, implemente também os seguintes métodos:
     sacar(self, valor): Retira um valor específico do saldo da conta, desde que haja
     saldo suficiente.
 """
+class ContaBancaria:
+    def __init__(self, numero_conta: int, titular: str, saldo:float) -> None:
+        self.numero_conta = numero_conta
+        self.titular = titular
+        self.saldo =  saldo
+
+    def __str__(self) -> str:
+        return f"Conta: {self.numero_conta}\nTitular: {self.titular}\nSaldo R$ {self.saldo:.2f}"
+    
+    def depositar(self, valor: float):
+        if valor <= 0:
+            print("O valor de deposito deve ser superior a zero!")
+        
+        elif valor > 0:
+            self.saldo += valor
+            print(f"Deposito: R${valor} realizado.\nNovo Saldo R${self.saldo:.2f}")
+
+    def sacar(self, valor: float):
+        if valor <= 0:
+            print("O valor de saque deve ser positivo.")
+
+        if self.saldo < valor:
+            print(f"Saldo insuficiente. O valor de saque (R${valor}) é maior do que o saldo atual (R${self.saldo:.2f}).")
+        else:
+            self.saldo -= valor
+            print(f"Saque de R${valor} realizado com sucesso.")
+            print(f"Novo saldo R${self.saldo:.2f}")
+        
+giovane = ContaBancaria(10101, "Giovane", 500)
+giovane.sacar(600)
